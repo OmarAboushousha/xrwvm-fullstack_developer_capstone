6cb998b697e44241086a0d8ce28c19717257a9e1
OmarAboushousha <oaboushousha99@gmail.com>
2025-11-24 16:34:54 -0500

    containerized application and deployed via kubernetes

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
+set -e  # Exit if any command fails
+set -x  # Print commands as they run (helps debugging in kubectl logs)
 
-# Make migrations and migrate the database.
-echo "Making migrations and migrating the database. "
+echo "Running Django migrations and collecting static files..."
+
 python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+python manage.py migrate --run-syncdb --noinput
 python manage.py collectstatic --noinput
+
+echo "Starting server..."
 exec "$@"
